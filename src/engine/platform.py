47b992f3c1c93465834c349d337f6ec5a2e3b403
Parya Bagheri <paryabagheri@yahoo.com>
2025-07-06 21:30:32 +0330

    modifying platform class

--- src/engine/platform.py
+++ src/engine/platform.py
@@ -1,13 +1,44 @@
 import pygame
+import config
 
-class Platform:
-    def __init__(self, x, y, width, height, color=(139, 69, 19), is_solid=True):
-        self.rect = pygame.Rect(x, y, width, height)
+class Platform(pygame.sprite.Sprite):
+    def __init__(self,game, x, y, width, height, type, color=(139, 69, 19), is_solid=True, image=None):
+        super().__init__()
+        self.game = game
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        if image:
+            self.image = image.convert()
+        else:
+            self.image = pygame.Surface((width, height))
+            self.image.fill(color)
+        self.rect = self.image.get_rect(topleft=(x,y))
         self.color = color
         self.is_solid = is_solid  # Determines if platform is solid for collision
-    
+        self.type = type
+        self.activated = False
+        self.visible = True
+        self.visibility_timer = 0
+        self.slowing_timer = 0
+
+    def update(self):
+        if self.activated :
+            self.visibility_timer += 1
+            if self.visibility_timer >= 180 :
+                self.visible = False
+
     def draw(self, screen, camera_x):
         # Draw platform with camera offset
-        draw_rect = pygame.Rect(self.rect.x - camera_x, self.rect.y, 
-                               self.rect.width, self.rect.height)
-        pygame.draw.rect(screen, self.color, draw_rect)
+        if self.visible :
+            pos_x = self.rect.x - camera_x
+            pos_y = self.rect.y
+            screen.blit(self.image, (pos_x, pos_y))
+
+
+
+
+
+        
+        
