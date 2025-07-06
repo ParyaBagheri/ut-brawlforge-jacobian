e244e116214c52b1e44e2b7bbaabae3280b6f575
Parya Bagheri <paryabagheri@yahoo.com>
2025-07-06 21:13:53 +0330

    Button fixes

--- src/engine/button.py
+++ src/engine/button.py
@@ -2,20 +2,38 @@ import pygame
 import sys
 
 class Button :
-    def __init__(self, game, image, pos, text_input, font, color, hovercolor):
+    def __init__(self, game, image, pos, text_input, font, fontsize, color, hovercolor):
         self.game = game
         self.image = image
+        self.font = pygame.font.Font("src/assets/fonts/"+ font + ".ttf", fontsize)
+        #self.font = pygame.font.SysFont('OCR A Extended', fontsize)
         self.x = pos[0]
         self.y = pos[1]
         self.text_input = text_input
         self.color = color
         self.hovercolor = hovercolor
-        self.text = text_input.render(text_input, True, color)
+        self.text = self.font.render(text_input, True, color)
         if self.image is None :
             self.image = self.text
         self.rect = self.image.get_rect(center=(self.x,self.y))
         self.text_rect = self.text.get_rect(center=(self.x,self.y))
 
-    def draw(self):
+    def draw(self, mouse_pos):
+        is_hovered = self.rect.collidepoint(mouse_pos[0], mouse_pos[1])
+        text_color = self.hovercolor if is_hovered else self.color
+        self.text = self.font.render(self.text_input, True, text_color)
         if self.image is not None:
-            screen.blit()
+            self.game.screen.blit(self.image, self.rect)
+        self.game.screen.blit(self.text, self.text_rect)
+    
+    def is_pressed(self, mouse_pos):
+        if mouse_pos[0] in range(self.rect.left, self.rect.right) and mouse_pos[1] in range(self.rect.top, self.rect.bottom):
+            return True
+        return False
+    
+    '''def changecolor(self, mouse_pos):
+        if mouse_pos[0] in range(self.rect.left, self.rect.right) and mouse_pos[1] in range(self.rect.top, self.rect.bottom):
+            self.text = self.font.render(self.text_input, True, self.hovercolor)
+        self.text = self.font.render(self.text_input, True, self.color)'''       
+
+    
