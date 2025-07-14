e0a5308a0cb0cf88e5110d9fa94e74fbb67012e2
Parya Bagheri <paryabagheri@yahoo.com>
2025-07-14 19:26:07 +0330

    adding level and data loader class

---
+++ src/engine/level.py
@@ -0,0 +1,31 @@
+from src.engine import data_loader
+
+class Level:
+    def __init__(self,name, game, winFunc=None, loseFunc=None):
+        self.name = name
+        self.game = game
+        #self.platforms = platforms
+        #self.entities = entities
+        self.background_layers = []
+        self.platforms = []
+        self.enemies = []
+        self.winFunc = winFunc
+        self.loseFunc = loseFunc
+        self.load()
+
+    def load(self):
+        data = data_loader.get_level_data(self.name, self.game)
+
+        self.background_layers = data["background_layers"]
+        self.platforms = data["platforms"]
+        self.enemies = data["enemies"]
+        #self.player = data["player"]
+    def won(self):
+        if self.winFunc is None:
+            return False
+        return self.winFunc()
+    def lost(self):
+        if self.loseFunc is None:
+            return False
+        return self.loseFunc()
+    
